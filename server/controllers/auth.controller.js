a31f8b9470251c1eb8d0e77ed4a86e3fb17d963c
yachana-chauhan <yachanac23@gmail.com>
2026-03-05 16:34:52 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const googleAuth = async (req,res) => {
         res.cookie("token" , token , {
             httpOnly:true,
             secure:true,
-            samesite:"none",
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
 
         })
