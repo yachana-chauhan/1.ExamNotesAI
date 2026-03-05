40f146e5911d432061c8b543acc3904ed3dd746c
yachana-chauhan <yachanac23@gmail.com>
2026-03-05 16:17:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://one-examnotesaiclient-7z55.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
